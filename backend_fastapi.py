d7af25be6bd9648081694f3524e2cbff99fa80cd
Coder2287569013 <yarovenkovova2008@gmail.com>
2025-01-06 18:39:32 -0330

    First commit

---
+++ backend_fastapi.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/media", StaticFiles(directory="media"), name="media")
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
