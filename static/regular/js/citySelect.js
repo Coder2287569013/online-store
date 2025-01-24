5fa4dd7d815900ec699190e18a50f3ec4407339b
Coder2287569013 <yarovenkovova2008@gmail.com>
2025-01-23 21:11:46 -0330

    Week #3: Dynamic search engine, will be improved

---
+++ static/regular/js/citySelect.js
@@ -0,0 +1,22 @@
+document.getElementById('region_id').addEventListener('change', function () {
+    const regionId = this.value;
+    const citySelect = document.getElementById('id_city');
+    citySelect.innerHTML = '<option value="">--- Select a city ---</option>';
+    if (regionId) {
+        fetch(`cities/?region_id=${regionId}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch cities');
+            }
+            return response.json();
+        })
+            .then(data => {
+                data.forEach(city => {
+                    const option = document.createElement('option');
+                    option.value = city.id;
+                    option.textContent = city.name;
+                    citySelect.appendChild(option);
+                });
+            });
+    }
+});
---
+++ static/regular/js/searchEngine.js
@@ -0,0 +1,16 @@
+document.getElementById('search-input').addEventListener('input', function () {
+    const query = this.value;
+    if (query.length > 2) {
+        fetch(`/search/suggestions/?q=${query}`)
+            .then(response => response.json())
+            .then(data => {
+                const suggestions = document.getElementById('suggestions');
+                suggestions.innerHTML = '';
+                data.forEach(suggestion => {
+                    const div = document.createElement('div');
+                    div.innerHTML = `<a href="${suggestion.url}">${suggestion.text}</a>`;
+                    suggestions.appendChild(div);
+                });
+            });
+    }
+});
